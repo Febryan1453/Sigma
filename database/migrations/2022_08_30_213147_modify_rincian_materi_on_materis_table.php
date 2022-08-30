58b2d725f2a7e640afd81d82f8067e3a914c78e5
Febryan1453 <brayenfebryan@gmail.com>
2022-08-30 21:33:09 +0700

    Fitur Materi

---
+++ database/migrations/2022_08_30_213147_modify_rincian_materi_on_materis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('materis', function(Blueprint $table){
+            $table->text('rincian_materi')->nullable()->change();
+            $table->text('link_materi')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
